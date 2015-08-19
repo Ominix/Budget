65e26306d763f2f1eb28ca3938f83fd439baccd0
Ominix <ryan.buck1989@gmail.com>
2015-08-19 00:40:50 -0400

    fixed issue where calcs was stuck in infinite loop

--- src/Calcs.java
+++ src/Calcs.java
@@ -1,26 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calcs {
 	//defining variables for the class
 	String incomeName = "", yOrN = "";
 	int income = 0;
-
-		
+	
+	ArrayList<String> incomeNameArray = new ArrayList<String>();
+	ArrayList<Integer> incomeArray = new ArrayList<Integer>();
 	Scanner scanner = new Scanner(System.in); //instantiating the scanner for use by the class
 	private Validation check = new Validation();
 	
 	public void getIncome(){		
-		System.out.print("Please enter the name of your your income: "); //asking for the name of the first income
-		incomeName = scanner.nextLine(); //accepting name of income
+		
+		int x = 0;
+		while(x != 1){
+			
+			System.out.print("Please enter the name of your your income: "); //asking for the name of the first income
+			incomeName = scanner.nextLine(); //accepting name of income
+			incomeNameArray.add(incomeName);
+			
+			System.out.print("How much income is generated from " + incomeName + ": $"); //asking for the amount of
+			income = Integer.parseInt(scanner.nextLine().trim()); //accepting amt of income
+			incomeArray.add(income);
+			
+			System.out.print("Would you like to add another income? Y or N: "); //checking if user would like to add another income
+			yOrN = scanner.nextLine();	//accepting answer
+			yOrN = check.properCase(yOrN); //changing y or n to uppercase
+			
+			if (yOrN.equals("N")){
+				x = 1;
+				System.out.println(incomeNameArray);
+				System.out.println(incomeArray);
+			}
+		}
 		
 		
-		System.out.print("How much income is generated from " + incomeName + ": $"); //asking for the amount of
-		income = Integer.parseInt(scanner.nextLine().trim()); //accepting amt of income
-		
-		System.out.print("Would you like to add another income? Y or N: "); //checking if user would like to add another income
-		yOrN = scanner.nextLine();	//accepting answer
-		yOrN = check.properCase(yOrN); //changing y or n to uppercase
 		
 	}
-
 }
